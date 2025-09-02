45c6591ef78c6da70ac79406a745406394791b82
Abby <abbysuyuyan@gmail.com>
2025-09-02 14:27:35 +0800

    Create Solana Liquidity - DAU by Protocol.sql

---
+++ Solana Liquidity - DAU by Protocol.sql
@@ -0,0 +1,20 @@
+WITH src AS (
+  SELECT
+    date_trunc('week', block_time) AS wk,
+    CASE
+      WHEN project IN ('raydium','raydium_launchlab') THEN 'raydium'
+      WHEN project = 'whirlpool'                      THEN 'orca'
+      ELSE project
+    END AS project,
+    trader_id
+  FROM dex_solana.trades
+  WHERE block_time >= date_add('week', -26, date_trunc('week', current_timestamp))
+    AND project IN ('raydium','raydium_launchlab','whirlpool','meteora','pumpswap','pumpdotfun','solfi')
+)
+SELECT
+  wk,
+  project,
+  COUNT(DISTINCT trader_id) AS dau
+FROM src
+GROUP BY 1,2
+ORDER BY wk, project;
