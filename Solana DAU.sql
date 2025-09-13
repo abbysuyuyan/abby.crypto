912ab1aafdad98d3d15973677a92357ee731b734
Abby <abbysuyuyan@gmail.com>
2025-09-13 18:42:40 +0800

    Update Solana DAU.sql

--- Solana DAU.sql
+++ Solana DAU.sql
@@ -5,34 +5,31 @@ params AS (
     date_trunc('day', now()) AS end_ts
 ),
 
-unique_daily_users AS (
-  SELECT DISTINCT
+daily_users AS (
+  SELECT
     date_trunc('day', block_time) AS day,
-    trader_id AS user
+    trader_id AS user,
+    MIN(block_time) AS first_trade_time
   FROM dex_solana.trades
   WHERE block_time >= (SELECT start_ts FROM params)
     AND block_time < (SELECT end_ts FROM params)
-),
-
-user_first_day AS (
-  SELECT 
-    user,
-    MIN(day) AS first_day
-  FROM unique_daily_users
-  GROUP BY user
+  GROUP BY 1, 2  
 ),
 
 daily_stats AS (
   SELECT
-    u.day,
-    COUNT(DISTINCT u.user) AS dau,
-    COUNT(DISTINCT f.user) AS new_users
-  FROM unique_daily_users u
-  LEFT JOIN user_first_day f ON u.day = f.first_day AND u.user = f.user
-  GROUP BY u.day
+    CAST(day AS DATE) AS day,
+    COUNT(DISTINCT user) AS dau,
+    COUNT(DISTINCT CASE 
+      WHEN day = date_trunc('day', first_trade_time) 
+      THEN user 
+    END) AS new_users
+  FROM daily_users
+  GROUP BY 1
 )
+
 SELECT
-  CAST(day AS DATE) AS day,
+  day,
   dau,
   new_users,
   SUM(new_users) OVER (ORDER BY day) AS cumulative_users
