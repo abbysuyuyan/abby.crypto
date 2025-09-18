9dfd9026cdf40aa83959b29cc5e486b3e89f2210
Abby <abbysuyuyan@gmail.com>
2025-09-18 19:19:38 +0800

    Update Solana Liquidity - Volume USD by Protocol.sql

--- Solana Liquidity - Volume USD by Protocol.sql
+++ Solana Liquidity - Volume USD by Protocol.sql
@@ -1,20 +1,14 @@
-WITH src AS (
-  SELECT
-    date_trunc('week', block_time) AS wk,
-    CASE
-      WHEN project IN ('raydium','raydium_launchlab') THEN 'raydium'
-      WHEN project = 'whirlpool'                      THEN 'orca'
-      ELSE project
-    END AS project,
-    amount_usd
-  FROM dex_solana.trades
-  WHERE block_time >= date_add('week', -26, date_trunc('week', current_timestamp))
-    AND project IN ('raydium','raydium_launchlab','whirlpool','meteora','pumpswap','pumpdotfun','solfi')
-)
 SELECT
-  wk,
-  project,
+  DATE(block_time) AS day,
+  CASE
+    WHEN project IN ('raydium', 'raydium_launchlab') THEN 'raydium'
+    WHEN project = 'whirlpool' THEN 'orca'
+    ELSE project
+  END AS project,
   SUM(amount_usd) AS volume_usd
-FROM src
-GROUP BY 1,2
-ORDER BY wk, project;
+FROM dex_solana.trades
+WHERE block_time >= CURRENT_DATE - INTERVAL '365' DAY
+  AND block_time < CURRENT_DATE
+  AND amount_usd > 0
+GROUP BY 1, 2
+ORDER BY 1 DESC, 3 DESC;
