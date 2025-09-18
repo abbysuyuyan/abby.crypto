6f4f8fd3187c841e9967e2f893ad460c7d8d5659
Abby <abbysuyuyan@gmail.com>
2025-09-18 18:34:05 +0800

    Update Solana Liquidity - Transaction by Protocol.sql

--- Solana Liquidity - Transaction by Protocol.sql
+++ Solana Liquidity - Transaction by Protocol.sql
@@ -1,19 +1,14 @@
-WITH src AS (
-  SELECT
-    date_trunc('week', block_time) AS wk,
-    CASE
-      WHEN project IN ('raydium','raydium_launchlab') THEN 'raydium'
-      WHEN project = 'whirlpool'                      THEN 'orca'
-      ELSE project
-    END AS project
-  FROM dex_solana.trades
-  WHERE block_time >= date_add('week', -26, date_trunc('week', current_timestamp))
-    AND project IN ('raydium','raydium_launchlab','whirlpool','meteora','pumpswap','pumpdotfun','solfi')
-)
 SELECT
-  wk,
-  project,
+  DATE(block_time) AS day,
+  CASE
+    WHEN project IN ('raydium', 'raydium_launchlab') THEN 'raydium'
+    WHEN project = 'whirlpool' THEN 'orca'
+    ELSE project
+  END AS project,
   COUNT(*) AS trades
-FROM src
-GROUP BY 1,2
-ORDER BY wk, project;
+FROM dex_solana.trades
+WHERE block_time >= CURRENT_DATE - INTERVAL '365' DAY
+  AND block_time < CURRENT_DATE
+  AND project IN ('raydium', 'raydium_launchlab', 'whirlpool', 'meteora', 'pumpswap', 'pumpdotfun', 'solfi')
+GROUP BY 1, 2
+ORDER BY 1 DESC, 3 DESC;
